fff51cbf36c60c547fe3c97e92ccf12f2a7a58f3
Anton <dr.dud34@gmail.com>
2023-07-16 16:33:43 +0300

    refactor: change chart case

--- src/theme/useTheme.ts
+++ src/theme/useTheme.ts
@@ -6,7 +6,7 @@ interface UseThemeResult {
     theme: Theme,
 }
 
-export const useTheme = () => {
+export const useTheme = (): UseThemeResult => {
     const {theme, setTheme} = useContext(ThemeContext)
 
     const toggleTheme = () => {
